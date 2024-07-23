7068b42de002b0e52b452997a842d99168b5a7c9
Francisco Gleidson Teixeira <03538064393@DIGITALCOLLEGE.LOCAL>
2024-07-23 12:01:11 -0300

    inital

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  );
+}
+
+export default App;
