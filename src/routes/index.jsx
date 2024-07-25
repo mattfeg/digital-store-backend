836188f83d35e9286c7282598a0ce26484c96756
Francisco Gleidson Teixeira <03538064393@DIGITALCOLLEGE.LOCAL>
2024-07-25 11:03:13 -0300

    feat: Base de rotas concluída

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,7 +8,7 @@ export const Paths = () => {
             <Routes>
                 <Route path="/" element={<PageLayout />}>
                     <Route index element={<Home />} />
-                    {/* <Route path="seu-caminho" element={seu componente} /> */}
+                    {/* <Route path="seu-caminho" element={seu componente } /> */}
                 </Route>
             </Routes>
         </BrowserRouter>
