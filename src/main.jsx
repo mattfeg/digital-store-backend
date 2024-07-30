4f9f38c44518cb6e4588b06e172e4779c92baeec
JeffLobao <jefferson_lobao@hotmail.com>
2024-07-30 11:23:56 -0300

    tela de cadasstro completo]

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import CadastroCompleto from './pages/cadastroCompleto/CadastroCompleto.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  // <App />
+  <CadastroCompleto />
 )
