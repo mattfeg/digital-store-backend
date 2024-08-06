93e25531daa543c15c1f7077d4172b1567a4af74
Matheus Ferreira <matheusmlfg@gmail.com>
2024-08-06 00:37:05 -0300

    Implementa Task Completa de Usuarios

--- index.js
+++ index.js
@@ -1,20 +1,85 @@
-const express =  require('express');
-const cors =  require('cors');
-
-const port = 8000;
-const app = express();
-
+const { logar } = require('./src/controllers/usuarioController')
+const { verificarToken } = require('./src/utils')
 const usuarioRoutes = require('./src/routes/usuarioRoute')
+const express =  require('express')
+const cors =  require('cors')
+const nodemailer = require('nodemailer')
 
-app.use(cors());
-app.use(express.json());
+const port = 8000
+const app = express()
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+})
+
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send("Documentação");
+    res.send("Documentação")
 });
 
-app.use('/usuarios', usuarioRoutes)
+app.use('/usuarios', verificarToken, usuarioRoutes)
+app.post('/login', async(req,res)=>{
+    res.send(await logar(req.body))
+})
+
+app.post('/req-recuperar-senha', async (req, res) => {
+    const usuarioEmail = req.body.usuario_email
+    const response = await executarSQL(`SELECT * FROM usuarios WHERE usuario_email = '${usuarioEmail}';`)[0]
+
+    if (!0<response.length) {
+        return res.status(400).json({ message: 'Email não encontrado' })
+    }
+
+    const token = jwt.encode({ usuarioEmail, exp: Date.now() + 3600000 }, process.env.SECRET)
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: usuario_email,
+        subject: 'Redefinição de Senha',
+        html: `<p>Para redefinir sua senha, clique no link abaixo:</p>
+               <a href="http://localhost:3000/recuperar-senha/${token}">Redefinir Senha</a>`
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).json({ message: 'Erro ao enviar email' })
+        }
+        res.status(200).json({ message: 'Email de redefinição de senha enviado' })
+    })
+})
+
+app.post('/recuperar-senha/:token', async (req, res) => {
+    const { token } = req.params
+    const { senha } = req.body
+
+    try {
+        const decoded = jwt.decode(token, process.env.SECRET)
+
+        if (decoded.exp <= Date.now()) {
+            return res.status(400).json({ message: 'Token expirado' })
+        }
+
+        const response = await executarSQL(`SELECT * FROM usuarios WHERE usuario_email = '${usuarioEmail}';`)[0]
+
+        if (!0<response.length) {
+            return res.status(400).json({ message: 'Usuário não encontrado' })
+        }
+
+        const senhaEncriptada = await bcrypt.hash(senha, 10)
+        await executarSQL(`UPDATE usuarios SET usuario_senha = '${senhaEncriptada}' WHERE usuario_email = '${usuarioEmail}';`)
+
+        return res.status(200).json({ message: 'Senha redefinida com sucesso' })
+    } catch (error) {
+        res.status(400).json({ message: 'Token inválido' })
+    }
+})
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-});
+    console.log(`http://localhost:${port}`)
+})
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,4 +1,7 @@
 const { executarSQL } = require('../database/index')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+require('dotenv').config()
 
 async function listarUsuarios(){
     return await executarSQL("SELECT * FROM usuarios;")
@@ -26,37 +29,67 @@ async function listarUmUsuario(id){
 }
 
 async function cadastrarUsuario(data){
-    await executarSQL(`INSERT INTO usuarios (
-        usuario_email,
-        usuario_senha,
-        usuario_nome,
-        usuario_cpf,
-        usuario_celular,
-        usuario_newsletter
-        ) VALUES (
-        '${data.usuario_email}',
-        '${data.usuario_senha}',
-        '${data.usuario_nome}',
-        '${data.usuario_cpf}',
-        '${data.usuario_celular}',
-        '${data.usuario_newsletter}'
+
+    const saltRounds = 10;
+    const senhaEncriptada = await bcrypt.hash(data.usuario_senha, saltRounds)
+    try {
+        const response = await executarSQL(`INSERT INTO usuarios (
+            usuario_email,
+            usuario_senha,
+            usuario_nome,
+            usuario_cpf,
+            usuario_celular,
+            usuario_newsletter
+            ) VALUES (
+            '${data.usuario_email}',
+            '${senhaEncriptada}',
+            '${data.usuario_nome}',
+            '${data.usuario_cpf}',
+            '${data.usuario_celular}',
+            '${data.usuario_newsletter}'
         );`)
-    return {
-        status: 200,
-        detail: "Usuario cadastrado com Sucesso.",
-        severity: "success"
+        
+        if(0 < response.affectedRows){
+            return {
+                status: 200,
+                detail: "Usuario cadastrado com sucesso.",
+                severity: "success"
+            }
+        }else{
+            return {
+                status: 422,
+                detail: "Usuario ja existe",
+                severity: "warn"
+            }
+        }
+    } catch (error) {
+        return {
+            status: 422,
+            detail: error.message,
+            severity: "danger"
+        }
     }
+
 }
 
 async function apagarUsuario(id){
     
     try {
-        await executarSQL(`DELETE FROM usuarios WHERE usuario_id = ${id}`)
-        return {
-            status: 200,
-            detail: "Usuario deletado.",
-            severity: "success"
+        const response = await executarSQL(`DELETE FROM usuarios WHERE usuario_id = ${id}`)
+        if(0 < response.affectedRows){
+            return {
+                status: 200,
+                detail: "Usuario apagado.",
+                severity: "success"
+            }
+        }else{
+            return {
+                status: 422,
+                detail: "Usuario não encontrado.",
+                severity: "success"
+            }
         }
+        
     } catch (error) {
         return {
             status: 422,
@@ -67,10 +100,14 @@ async function apagarUsuario(id){
 }
 
 async function editarUsuario(data){
+
+    const saltRounds = 10;
+    const senhaEncriptada = await bcrypt.hash(data.usuario_senha, saltRounds)
+
     try {
-        await executarSQL(`UPDATE usuarios SET
+        const response = await executarSQL(`UPDATE usuarios SET
         usuario_email = "${data.usuario_email}",
-        usuario_senha = "${data.usuario_senha}",
+        usuario_senha = "${senhaEncriptada}",
         usuario_nome = "${data.usuario_nome}",
         usuario_cpf = "${data.usuario_cpf}",
         usuario_celular = "${data.usuario_celular}",
@@ -78,11 +115,21 @@ async function editarUsuario(data){
         WHERE
         usuario_id = ${data.usuario_id}
         ;`)
-        return {
-            status: 200,
-            detail: "Usuario atualizado.",
-            severity: "success"
+
+        if(0 < response.affectedRows){
+            return {
+                status: 200,
+                detail: "Usuario atualizado.",
+                severity: "success"
+            }
+        }else{
+            return {
+                status: 422,
+                detail: "Usuario não encontrado.",
+                severity: "warn"
+            }
         }
+        
     } catch (error) {
         return {
             status: 422,
@@ -92,10 +139,62 @@ async function editarUsuario(data){
     }
 }
 
+async function logar(data){
+    try {
+        if(data.usuario_email == "" || !data.usuario_email){
+            throw new Error("Campo email é obrigatório!")
+        }
+
+        if(data.usuario_senha == "" || !data.usuario_senha){
+            throw new Error("Campo senha é obrigatório!")
+        }
+
+        const response = await executarSQL(`SELECT * FROM usuarios WHERE usuario_email = '${data.usuario_email}';`)
+
+        if(0 < response.length){
+            const usuario = response[0]
+            const aSenhaEstaCorreta = await bcrypt.compare(data.usuario_senha, usuario.usuario_senha)
+            if (aSenhaEstaCorreta) {
+                console.log('Senha correta!')
+                const token = jwt.sign({
+                    data: usuario.usuario_id
+                }, process.env.SECRET, { expiresIn: '1h' })
+                return {
+                    usuario_nome: usuario.usuario_nome,
+                    usuario_email: usuario.usuario_email,
+                    token
+                }
+            } else {
+                console.log('Senha incorreta!')
+                return {
+                    status: 422,
+                    detail: "Usuário ou senha incorretos.",
+                    severity: "warn"
+                }
+            }
+        }else{
+            return {
+                status: 422,
+                detail: "Usuário não encontrado.",
+                severity: "warn"
+            }
+        }
+
+        
+    } catch (error) {
+        return {
+            status: 422,
+            detail: error.message,
+            severity: "error"
+        }
+    }
+}
+
 module.exports = {
     listarUsuarios,
     listarUmUsuario,
     cadastrarUsuario,
     apagarUsuario,
-    editarUsuario
+    editarUsuario,
+    logar
 }
